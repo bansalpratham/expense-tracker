102c3fdcae22cd20b0538a7e8319405d99fec769
bansalpratham <prathambansal200@gmail.com>
2026-08-15 17:23:20 +0530

    feat: enhance category management with add, get, and delete functionalities

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,9 +1,108 @@
+import pool from "../db/db.js";
+
 const addCategory = async (req,res)=>{
     try {
         
+        const userId = req.user.id
+
+        if (!userId)
+        {
+           return res.status(400).json("UserId didn't found")
+        }
+
+        const user = await pool.query("SELECT * FROM users WHERE id=$1",[userId])
+
+       if (user.rows.length === 0)
+{
+    return res.status(400).json("User didn't found")
+}
+
+    const {categoryName} = req.body
+
+    if (!categoryName)
+    {
+       return res.status(400).json("categoryName not found")
+    }
+
+    const category = await pool.query(
+    "SELECT * FROM categories WHERE name=$1 AND user_id=$2",
+    [categoryName, userId]
+)
+    
+    if (category.rows.length > 0)
+    {
+        return res.status(400).json("Category already exists")
+    }
+    
+        const result = await pool.query(
+    "INSERT INTO categories(name,user_id) VALUES($1,$2) RETURNING id,name,user_id",
+    [categoryName, userId]
+)
+    
+    return res.status(201).json(result.rows[0])
+
     } catch (error) {
-        
+        console.error(error);
+    res.status(500).json({ error: "AddCategory Error" });
     }
 }
 
-export {addCategory};
+const getCategory = async (req,res)=>{
+    try {
+        
+        const userId = req.user.id
+
+        if (!userId)
+        {
+           return res.status(400).json("UserId didn't found")
+        }
+
+        const user = await pool.query("SELECT * FROM users WHERE id=$1",[userId])
+
+         if (user.rows.length === 0)
+{
+    return res.status(400).json("User didn't found")
+}
+
+    const result = await pool.query("SELECT * FROM categories WHERE user_id=$1",[userId])
+
+    return res.status(201).json(result.rows)
+
+    } catch (error) {
+        console.error(error);
+    res.status(500).json({ error: "GetCategory Error" });
+    }
+}
+
+const deleteCategory = async(req,res) =>{
+    try {
+        const { categoryId } = req.params
+
+const userId = req.user.id
+
+        if (!userId)
+        {
+           return res.status(400).json("UserId didn't found")
+        }
+
+        if (!categoryId)
+        {
+            return res.status(400).json("CategoryId didn't found")
+        }
+
+       const result = await pool.query("DELETE FROM categories WHERE id=$1 AND user_id=$2",[categoryId,userId])
+
+        if (result.rows.length === 0)
+{
+    return res.status(404).json("Category not found")
+}
+
+return res.status(200).json(result.rows[0])
+
+    } catch (error) {
+        console.error(error);
+    res.status(500).json({ error: "DeleteCategory Error" });
+    }
+}
+
+export {addCategory , getCategory ,deleteCategory};
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 import express, { Router } from 'express'
-import { addCategory } from '../controllers/categoryController.js'
+import { addCategory, deleteCategory, getCategory } from '../controllers/categoryController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const router2 = Router()
 
 router2.post("/addCategory", authMiddleware ,addCategory)
+router2.get("/getCategory", authMiddleware ,getCategory)
+router2.delete("/deleteCategory/:categoryId", authMiddleware, deleteCategory)
 
 export default router2
