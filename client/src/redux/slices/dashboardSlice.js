45636ca17ef1b0ef2299c7bb2f722223acf07047
bansalpratham <prathambansal200@gmail.com>
2026-08-19 17:20:11 +0530

    feat: add getDashboard async thunk for fetching dashboard data

--- client/src/redux/slices/dashboardSlice.js
+++ client/src/redux/slices/dashboardSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import api from "../../api/api"
 
 const initialState = {
     totalSpending: 0,
@@ -18,14 +19,43 @@ const initialState = {
     categorySpending: []
 }
 
+
+export const getDashboard = createAsyncThunk(
+    "dashboard/getDashboard",
+
+    async () => {
+        const response = await api.get("/dashboard")
+
+        return response.data
+    }
+)
+
+
 const dashboardSlice = createSlice({
-  name: "dashboard",
 
-  initialState,
+    name: "dashboard",
 
-  reducers: {
+    initialState,
 
-  }
+    reducers: {},
+
+    extraReducers: (builder) => {
+
+        builder
+
+        .addCase(getDashboard.fulfilled, (state, action) => {
+
+            state.totalSpending = action.payload.totalSpending
+
+            state.monthly = action.payload.monthly
+
+            state.weekly = action.payload.weekly
+
+        })
+
+    }
+
 })
+
 
 export default dashboardSlice.reducer
