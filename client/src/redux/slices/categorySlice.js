01cd6bf8660429096adb18ac7d5495a26bdd7f6e
bansalpratham <prathambansal200@gmail.com>
2026-08-19 16:26:24 +0530

    feat: add deleteCategory and getCategorySpending async thunks to category slice

--- client/src/redux/slices/categorySlice.js
+++ client/src/redux/slices/categorySlice.js
@@ -2,14 +2,14 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import api from "../../api/api"
 
 const initialState = {
-    categories: []
+    categories: [],
+    categorySpending: []
 }
 
 export const getCategories = createAsyncThunk(
     "category/getCategories",
 
     async () => {
-
         const response = await api.get("/getCategory")
 
         return response.data
@@ -20,10 +20,31 @@ export const addCategory = createAsyncThunk(
     "category/addCategory",
 
     async (categoryName) => {
-
         const response = await api.post("/addCategory", {
             categoryName
         })
+
+        return response.data
+    }
+)
+
+export const deleteCategory = createAsyncThunk(
+    "category/deleteCategory",
+
+    async (categoryId) => {
+        const response = await api.delete(
+            `/deleteCategory/${categoryId}`
+        )
+
+        return response.data
+    }
+)
+
+export const getCategorySpending = createAsyncThunk(
+    "category/getCategorySpending",
+
+    async () => {
+        const response = await api.get("/getCategorySpending")
 
         return response.data
     }
@@ -39,6 +60,7 @@ const categorySlice = createSlice({
     extraReducers: (builder) => {
 
         builder
+
             .addCase(getCategories.fulfilled, (state, action) => {
                 state.categories = action.payload
             })
@@ -46,6 +68,17 @@ const categorySlice = createSlice({
             .addCase(addCategory.fulfilled, (state, action) => {
                 state.categories.push(action.payload)
             })
+
+            .addCase(deleteCategory.fulfilled, (state, action) => {
+                state.categories = state.categories.filter(
+                    category => category.id !== action.payload.id
+                )
+            })
+
+            .addCase(getCategorySpending.fulfilled, (state, action) => {
+                state.categorySpending = action.payload
+            })
+
     }
 })
 
