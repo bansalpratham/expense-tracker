2cce2db82acec8e433077fe16d048848ca555948
bansalpratham <prathambansal200@gmail.com>
2026-08-25 12:40:56 +0530

    feat: update API base URL to use environment variable for flexibility

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://expense-tracker-pxhb.onrender.com"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 api.interceptors.request.use((config) => {
@@ -13,6 +13,7 @@ api.interceptors.request.use((config) => {
     }
 
     return config
+
 })
 
 export default api
