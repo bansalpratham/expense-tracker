ae5b41a9f2fcf3751579b6f3f4bbc44bdbf47063
bansalpratham <prathambansal200@gmail.com>
2026-08-25 13:48:36 +0530

    feat: add SSL configuration to database connection for enhanced security

--- server/db/db.js
+++ server/db/db.js
@@ -4,11 +4,14 @@ dotenv.config();
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
